da7ab0cc536f4c521aabab5c8055b5053920e475
Ahmet AK <ahmetak@gibir.com.tr>
2022-11-05 15:15:27 +0300

    Floating Numbers Support

--- turkish-number-to-word.js
+++ turkish-number-to-word.js
@@ -4,38 +4,50 @@ function numberToTurkishWords(s) {
 	var on = ['on', 'yirmi', 'otuz', 'kırk', 'elli', 'altmış', 'yetmiş', 'seksen', 'doksan'];
 	var nth = ['', 'bin', 'milyon', 'milyar', 'trilyon'];
   	var str = String(s);
-  	var len = str.length;
+  	var strPart = str.split('.');
+  	var text = [];
+  	for(var p in strPart){
+	  	var len = strPart[p].length;
+	  	if(len == 1){
+	  		text.push(bir[parseInt(strPart[p])]);
+	  		continue;
+	  	}
 
-  	if(len == 1){
-  		return bir[s];
-  	}
+	  	var results = [];
+	  	var perThird = [];
+	  	for(var i = 0; i < len; i++){
+	  		per = parseInt(i/3);
+	  		if(i%3 == 0){
+	  			perThird.push([]);
+	  		}
+	  		perThird[per].push(strPart[p][len-i-1]);
+	  	}
+	  	
+	  	for(var i in perThird){
+	  		var result = [];
+	  		var arr = perThird[i];
+	  		if(arr[0]){
+	  			if(arr[0] > 0){
+	  				result.push(bir[arr[0]]);
+	  			}
+	  		}
 
-  	var results = [];
-  	var perThird = [];
-  	for(var i = 0; i < len; i++){
-  		per = parseInt(i/3);
-  		if(i%3 == 0){
-  			perThird.push([]);
-  		}
-  		perThird[per].push(str[len-i-1]);
-  	}
-  	
-  	for(var i in perThird){
-  		var result = [];
-  		var arr = perThird[i];
-  		if(arr[0]){
-  			result.push(bir[arr[0]]);
-  		}
+	  		if(arr[1]){
+	  			if(arr[1] > 0){
+	  				result.push(on[arr[1]-1]);
+	  			}
+	  		}
 
-  		if(arr[1]){
-  			result.push(on[arr[1]-1]);
-  		}
+	  		if(arr[2]){
+	  			if(arr[2] > 0){
+	  				result.push((arr[2] == 1 ? '' : bir[arr[2]] + " ") + "yüz"); //bir yüz fix
+	  			}
+	  		}
 
-  		if(arr[2]){
-  			result.push((arr[2] == 1 ? '' : bir[arr[2]] + " ") + "yüz"); //bir yüz fix
-  		}
+	  		results.push(result.reverse().join(' ') + (i > 0 ? (" " + nth[i]) : ''));
+	  	}
+	  	text.push(results.reverse().join(' '));
+	}
 
-  		results.push(result.reverse().join(' ') + (i > 0 ? (" " + nth[i]) : ''));
-  	}
-  	return results.reverse().join(' ');
+	return text.join(' nokta ');
 }
